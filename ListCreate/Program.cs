baed1cf75f9d2e28a64690471312719e4cf632c7
HristoHakera123 <hrisi.belezirev@gmail.com>
2024-02-19 14:04:54 +0200

    Code from the school

---
+++ ListCreate/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ListCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете списък от зветове (done за край)");
+            List<string> colorList = new List<string>();
+            string? answer = Console.ReadLine();
+            while (answer != "done")
+            {
+                colorList.Add(answer);
+                answer = Console.ReadLine();
+            }
+            //for(int i = 0; i < colorList.Count; i++)
+            //{
+            //    Console.WriteLine(colorList[i]);
+            //}
+            foreach(string chervenokvadratcheobikaljashtoizspisuka in colorList)
+            {
+                Console.WriteLine(chervenokvadratcheobikaljashtoizspisuka);
+            }
+
+            Console.WriteLine($"Choose which element to delete (from 0-{colorList.Count-1})");
+            colorList.RemoveAt(int.Parse(Console.ReadLine()));
+
+            foreach (string chervenokvadratcheobikaljashtoizspisuka in colorList)
+            {
+                Console.WriteLine(chervenokvadratcheobikaljashtoizspisuka);
+            }
+        }
+    }
+}
